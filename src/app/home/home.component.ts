f9983be73277c7dc0bb7d19d715229314decd127
Sharvi Naik <sharvinaik@gmail.com>
2018-03-30 09:22:48 +0530

    initial commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { finalize } from 'rxjs/operators';
+import {HttpClient} from '@angular/common/http';
+
+import { QuoteService } from './quote.service';
+import {map} from 'rxjs/operators';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  images: Array<string>;
+  quote: string;
+  isLoading: boolean;
+
+  constructor(private quoteService: QuoteService,private _http: HttpClient) { }
+
+  ngOnInit() {
+    this.isLoading = true;
+    this.quoteService.getRandomQuote({ category: 'dev' })
+      .pipe(finalize(() => { this.isLoading = false; }))
+      .subscribe((quote: string) => { this.quote = quote; });
+     this.images = ['assets/image2.jpg','assets/image1.jpg'];
+  }
+  private _randomImageUrls(images: Array<{id: number}>): Array<string> {
+    return [1, 2, 3].map(() => {
+      const randomId = images[Math.floor(Math.random() * images.length)].id;
+      return `https://picsum.photos/900/500?image=${randomId}`;
+    });
+  }
+
+}
